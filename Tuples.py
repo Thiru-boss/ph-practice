44f4e13da6584166a3601f9e16f54c6ab0ffdc4b
Thiru-boss <maran28599@gmail.com>
2025-01-26 19:45:43 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,29 @@
+#Tuples
+'''buying fruits'''
+from operator import index
+
+fruits = ("mango","banana","apple")
+print(fruits)
+print('total fruits:', len(fruits))
+
+'''buying one more fruit'''
+fruits = fruits + ('kiwi',)
+
+#using Traversal to list the fruits
+for i in fruits:
+    print(i)
+print()
+
+'''fruits mentions as kg'''
+fruits = (2,7,10,4)
+
+'''finding in min and max of fruits kgs'''
+print('min kg:', min(fruits))
+print('max kg:', max(fruits))
+
+'''finding the order of the apple'''
+print('order of apple:', fruits.index(10))
+
+'''Count of how many times kiwi was bought'''
+print('Count kiwi:', fruits.count(4))
+
