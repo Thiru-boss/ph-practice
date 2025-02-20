887dd1db7cac009a1d24daf008f2df46b0283397
Thiru-boss <maran28599@gmail.com>
2025-02-20 13:56:39 +0530

    Add files via upload

---
+++ package/cal.py
@@ -0,0 +1,9 @@
+#calculator fot modules
+def div(a,b):
+    return a/b
+
+def mul(a,b):
+    return a*b
+
+def power(a,b):
+    return a**b
---
+++ package/calculator.py
@@ -0,0 +1,8 @@
+#calculator fot modules
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+
